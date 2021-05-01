4f53befce833c32cebadf42438dcbce82eaa6693
Jaya-272009 <66358077+Jaya-272009@users.noreply.github.com>
2021-05-02 03:10:03 +0530

    Cure column added

--- src/Components/InfoCards/Cure.js
+++ src/Components/InfoCards/Cure.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import { Col, Row } from 'antd'
+import classes from './InfoCard.module.css'
+import hot from '../../Assets/hot.svg'
+import oximeter from '../../Assets/oximeter.png'
+import vitamins from "../../Assets/vitamin.png"
+import milkBox from '../../Assets/milk-box.png'
 
 export default function Cure() {
     return (
-        <div>
-            Cure jaya
-        </div>
+        <Row gutter={[32, 32]} style={{flexDirection:'row-reverse'}} justify='center'>
+            <Col lg={8} md={11} sm={11} xs={20}>
+                <Row justify="center">
+                    <Col className={classes.SymptompsIcon} span={10}>
+                        <img src={hot} />
+                    </Col>
+                    <Col className={classes.SymptompsIcon} span={10}>
+                        <img src={oximeter} />
+                    </Col>
+                </Row>
+                <Row justify="center">
+                    <Col className={classes.SymptompsIcon} span={10}>
+                        <img src={vitamins} />
+                    </Col>
+                    <Col className={classes.SymptompsIcon} span={10}>
+                        <img src={milkBox} />
+                    </Col>
+                </Row>
+
+            </Col>
+            <Col lg={8} md={11} sm={11} xs={20}>
+                <div className={classes.Card2}>
+                    <h1>Cures</h1>
+                    <p>We can fight with COVID-19 on our own.We have to do some basic thing in our daily life.Some of these basic things are mentioned below:</p>
+                    <ul>
+                        <li>Steam</li>
+                        <li>Oximeter</li>
+                        <li>Consume fiber</li>
+                        <li>Vitamins</li>
+                        <li>Turmeric Milk</li>
+                    </ul>
+                </div>
+            </Col>
+        </Row>
     )
 }
