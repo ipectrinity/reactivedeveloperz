07231b1ae434c43c763645189fbc9b8e7bffb61f
souro12345 <52583823+souro12345@users.noreply.github.com>
2021-05-02 14:30:02 +0530

    Minor Changes

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>
+    <div id="footer">
       <div className={classes.footer}>
         <div className={classes.lFooter}>
           <div className={classes.head}>
@@ -14,10 +14,10 @@ const Footer = () => {
               style={{ height: "50px", width: "50px" }}
               alt='Covid Relief Logo'
             />
-            <div>
+            <div style={{ marginLeft: "10px" }}>
               <h1>Covid Relief</h1>
               <p>
-              Covid Relief is a one stop solution in this tough times..
+                Covid Relief is a one stop solution in this tough times..
               </p>
             </div>
           </div>
@@ -26,38 +26,39 @@ const Footer = () => {
           <li style={{ marginTop: "" }}>
             <h2>Services</h2>
             <ul className={classes.box}>
-              <li href='#'>Doctors</li>
-              <li href='#'>OxygenCylinders</li>
-              <li href='#'>Food</li>
+              <Link to='/volunteer'><li href=''>Contact Volunteer</li></Link>
+              <Link to='/oxygenCylinders'><li href=''>Get Oxygen Cylinders</li></Link>
+              <Link to='/food'><li href=''>Get Food</li></Link>
+             
             </ul>
           </li>
           <li className={classes.features}>
             <h2>Registration</h2>
             <ul className={classes.box + classes.hBox}>
-              <li href='#'>Doctors</li>
-              <Link to = {{pathname:"/registration"}}><li href='#'>Patients</li></Link>
-              <li href='#'>Donors</li>
-              <li href='#'>Recipient</li>
+              <Link to={{ pathname: "/register/donor" }}><li href='#'>Donor</li></Link>
+              <Link to={{ pathname: "/register/recipient" }}><li href='#'>Recipient</li></Link>
+              <Link to={{ pathname: "/register/food-supply" }}><li href='#'>Food Supplier</li></Link>
+              <Link to={{ pathname: "/register/Oxygen-cylinders-supply" }}><li href='#'>Oxygen Cylinders Supplier</li></Link>
             </ul>
           </li>
           <li>
             <h2>Developers</h2>
             <ul className={classes.box}>
               <div>
-              <a target='_blank' href="https://www.linkedin.com/in/sourodeep-ghosh-roy-828861208/">Sourodeep Ghosh</a>
+                <a target='_blank' href="https://www.linkedin.com/in/sourodeep-ghosh-roy-828861208/">Sourodeep Ghosh</a>
               </div>
               <div>
-              <a target='_blank' href="https://www.linkedin.com/in/ritikgupta1982/">Ritik Gupta</a>
+                <a target='_blank' href="https://www.linkedin.com/in/ritikgupta1982/">Ritik Gupta</a>
               </div>
               <div>
-              <a target='_blank' href="https://www.linkedin.com/in/disha-bahal-20424a1aa/">Disha Bahal</a>
+                <a target='_blank' href="https://www.linkedin.com/in/disha-bahal-20424a1aa/">Disha Bahal</a>
               </div>
               <div>
-              <a target='_blank' href="https://www.linkedin.com/in/jaya-gaur-473ba4190/">Jaya Gaur</a>
+                <a target='_blank' href="https://www.linkedin.com/in/jaya-gaur-473ba4190/">Jaya Gaur</a>
               </div>
-              
-              
-              
+
+
+
             </ul>
           </li>
         </ul>
