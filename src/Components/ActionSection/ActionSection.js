1c23d0daaea710dc2528a0c47d80e68c12892f49
souro12345 <52583823+souro12345@users.noreply.github.com>
2021-05-02 14:04:05 +0530

    Submit Page Updated

--- src/Components/ActionSection/ActionSection.js
+++ src/Components/ActionSection/ActionSection.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Col, Row, Button } from "antd";
 import classes from "../../Containers/Home/Home.module.css";
 import plasma from "../../Assets/blood.svg"
@@ -6,82 +6,115 @@ import oxygen from "../../Assets/oxygen-tank.svg"
 import food from "../../Assets/spoon-fork-lunch-bag-box.svg"
 import lab from "../../Assets/flask.svg"
 import volunteer from "../../Assets/volunteer.svg"
+import Modal from "antd/lib/modal/Modal";
 
 
 
 const ActionSection = (props) => {
-  
+
   const changeRoute = (route) => {
     console.log("Hello")
     props.history.push(route);
   };
+  const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
   return (
-    <Row justify='center'>
-      <Col lg={8} md={11} sm={11} xs={20}>
-        <div className={classes.actionCard}>
-          <p style={{ textAlign: "left" }}>
-            {" "}
+    <>
+      <Row justify='center'>
+        <Col lg={8} md={11} sm={11} xs={20}>
+          <div className={classes.actionCard}>
+            <p style={{ textAlign: "left" }}>
+              {" "}
             What are you looking for?
           </p>
-          <Button
-            onClick={() => changeRoute("register/recipient")}
-            block
-            className={classes.actionButton}>
-            Looking for Plasma Donors
+            <Button
+              onClick={() => changeRoute("register/recipient")}
+              block
+              className={classes.actionButton}>
+              Looking for Plasma Donors
           </Button>
-          <Button 
-            onClick={() => changeRoute("/oxygenCylinders")}
-            block
-            className={classes.actionButton2}>
-            Looking for oxygen cylinder
+            <Button
+              onClick={() => changeRoute("/oxygenCylinders")}
+              block
+              className={classes.actionButton2}>
+              Looking for oxygen cylinder
           </Button>
-          <Button
-            onClick={() => changeRoute("/food")}
-            block
-            className={classes.actionButton2}>
-            Looking for food delivery
+            <Button
+              onClick={() => changeRoute("/food")}
+              block
+              className={classes.actionButton2}>
+              Looking for food delivery
           </Button>
-          <Button
-            onClick={() => changeRoute("/volunteer")}
-            block
-            className={classes.actionButton2}>
-            Looking for volunteers
+            <Button
+              onClick={() => changeRoute("/volunteer")}
+              block
+              className={classes.actionButton2}>
+              Looking for volunteers
           </Button>
-        </div>
-      </Col>
-      <Col lg={8} md={11} sm={11} xs={20}>
-        <div className={classes.actionCard}>
-          <p style={{ textAlign: "left" }}> Come forward to Help!!</p>
-          <div className={classes.iconBox}>
-
-          <Col onClick={() => changeRoute("register/Donor")} style={{backgroundColor:'#2F61F5'}} span={11} className={classes.selectedcheckBox1}>
-              <label style={{ textAlign: 'center' }} for="fever">
-              <img className={classes.image} src={plasma} />
-              <div style={{color:'white'}} className={classes.text}> Plasma Donor </div>
-              </label>
-            </Col>
-            <Col onClick={() => changeRoute("register/Oxygen-cylinders-supply")} span={11} className={classes.selectedcheckBox1}>
-              <label style={{ textAlign: 'center' }} for="fever">
-              <img className={classes.image} src={oxygen} />
-              <div  className={classes.text}>Provide Oxygen Cylinder </div>
-              </label>
-            </Col>
-            <Col onClick={() => changeRoute("register/food-supply")} span={11} className={classes.selectedcheckBox1}>
-              <label style={{ textAlign: 'center' }} for="fever">
-              <img className={classes.image} src={food} />
-              <div  className={classes.text}> Food Delivery </div>
-              </label>
-            </Col>
-            <Col onClick={() => changeRoute("register/volunteer")} span={11} className={classes.selectedcheckBox1}>
-              <label style={{ textAlign: 'center' }} for="fever">
-              <img className={classes.image} src={volunteer} />
-              <div  className={classes.text}> Volunteer </div>
-              </label>
-            </Col>
           </div>
+        </Col>
+        <Col lg={8} md={11} sm={11} xs={20}>
+          <div className={classes.actionCard}>
+            <p style={{ textAlign: "left" }}> Come forward to Help!!</p>
+            <div className={classes.iconBox}>
+
+              <Col onClick={() => changeRoute("register/Donor")} style={{ backgroundColor: '#2F61F5' }} span={11} className={classes.selectedcheckBox1}>
+                <label style={{ textAlign: 'center' }} for="fever">
+                  <img className={classes.image} src={plasma} />
+                  <div style={{ color: 'white' }} className={classes.text}> Plasma Donor </div>
+                </label>
+              </Col>
+              <Col onClick={() => changeRoute("register/Oxygen-cylinders-supply")} span={11} className={classes.selectedcheckBox1}>
+                <label style={{ textAlign: 'center' }} for="fever">
+                  <img className={classes.image} src={oxygen} />
+                  <div className={classes.text}>Provide Oxygen Cylinder </div>
+                </label>
+              </Col>
+              <Col onClick={() => changeRoute("register/food-supply")} span={11} className={classes.selectedcheckBox1}>
+                <label style={{ textAlign: 'center' }} for="fever">
+                  <img className={classes.image} src={food} />
+                  <div className={classes.text}> Food Delivery </div>
+                </label>
+              </Col>
+              <Col onClick={showModal} span={11} className={classes.selectedcheckBox1}>
+                <label style={{ textAlign: 'center' }} for="fever">
+                  <img className={classes.image} src={volunteer} />
+                  <div className={classes.text}> Volunteer </div>
+                </label>
+              </Col>
+            </div>
+          </div>
+        </Col>
+      </Row>
+      <Modal
+        title={null}
+        className={classes.modal}
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        footer={null}
+        closeIcon={<ion-icon name="close-outline"></ion-icon>}>
+        <div className={classes.title1}>
+          <p>Thank you for Taking The Initiative</p>
         </div>
-      </Col>
-    </Row>
+        <br /><br />
+        <a href="tel:9871597556"><Button  block className={classes.actionButton}>
+          Please Contact at 9871597556
+            </Button></a>
+    
+      </Modal>
+    </>
   );
 };
 
--- src/Components/SubmitPage/SubmitPage.js
+++ src/Components/SubmitPage/SubmitPage.js
@@ -1,9 +1,39 @@
+import { Button, Col, message, Row } from 'antd'
 import React from 'react'
+import classes from './SubmitPage.module.css'
+import icon from "../../Assets/Congratulations1.svg"
 
-export default function SubmitPage() {
+const copytoClipboard=(text)=>{
+    navigator.clipboard.writeText(text)
+    message.success("Reference Id Copied")
+}
+
+
+
+
+
+export default function SubmitPage(props) {
     return (
-        <div>
-            SubmitPage
-        </div>
+        <Row justify="center">
+            <Col style={{ backgroundColor: "white", padding: "20px", marginTop: "40px" }} md={6} sm={16} xs={18}>
+                <div className={classes.title1}>
+                    <h1>Congratulations</h1>
+                </div>
+                <br />
+                <img src={icon} style={{ height: "130px", width: "100%" }} />
+                <br /><br />
+                <div className={classes.content}>
+                    <h1>Take Screenshot or copy this Reference Id</h1>
+                    <p onClick={() => copytoClipboard(props.match.params.docId)} style={{ color: "green", textAlign: "center", fontWeight: "bold", cursor: "pointer" }}>{props.match.params.docId}</p>
+                    
+                    <h1>We have registered you successfully</h1>
+                    <h2>We Will Reach Out To You As Per Required</h2>
+                </div>
+                <br /><br />
+                <Button onClick={() => props.history.push("/")} block className={classes.actionButton}>
+                    Go to Homescreen
+                </Button>
+            </Col>
+        </Row>
     )
 }
