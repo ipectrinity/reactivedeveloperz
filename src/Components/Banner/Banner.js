c7111257ba6bb46421e2da7b641b5968d019d184
Ritik Gupta <ritikgupta1982@gmail.com>
2021-05-02 17:53:14 +0530

    Banner Responsiveness Complete

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -27,21 +27,21 @@ const Banner = () => {
           <div className={classes.statsSection}>
             <div className={classes.imageStat}>
               <img src={life} alt='life icon' />
-              <p style={{ fontSize: "30px" }}>Recoveries per day</p>
+              <p className={classes.category}>Recoveries per day</p>
               <p>
                 <IncrementCount number={"300"} duration={"2"} text={"K"} />
               </p>
             </div>
             <div className={classes.imageStat}>
               <img src={vaccination} alt='vaccine icon' />
-              <p style={{ fontSize: "30px" }}>Vaccination per day</p>
+              <p className={classes.category}>Vaccination per day</p>
               <p>
                 <IncrementCount number={"3640"} duration={"2"} text={"K"} />
               </p>
             </div>
             <div className={classes.imageStat}>
               <img src={volunteers} alt='volunteer icon' />
-              <p style={{ fontSize: "30px" }}>Volunteers Helping</p>
+              <p className={classes.category}>Volunteers</p>
               <p>
                 <IncrementCount number={"500"} duration={"2"} text={""} />
               </p>
